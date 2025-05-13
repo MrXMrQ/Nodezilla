3c32e6d8dfce145d1984f46597ceebf450d2174e
Marvin Bäumer <m.baeumee.industries@gmail.com>
2025-05-13 13:20:07 +0200

    update: graph reader
    load function and data prop

--- json_reader.py
+++ json_reader.py
@@ -1,3 +1,27 @@
+import json
+import os
+
+
 class JSONReader:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, path: str = "graph.json") -> None:
+        if not isinstance(path, str):
+            raise TypeError(
+                f"Invalid type for 'path': expected 'str', got '{type(path)}' instead."
+            )
+
+        self._path: str = path
+        self._data: dict = self.load()
+
+    def load(self) -> dict:
+        try:
+            with open(self._path, "r") as file:
+                return json.load(file)
+
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                f"The specified file does not exist: '{self._path}'"
+            )
+
+    @property
+    def data(self) -> dict:
+        return self._data
